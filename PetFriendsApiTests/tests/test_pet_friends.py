e2e7629b759e6096cc3a81de712943d4cd7dca2b
Aleksey Melnikov <melnikkov@gmail.com>
2020-11-06 10:07:06 +0300

    -

---
+++ PetFriendsApiTests/tests/test_pet_friends.py
@@ -0,0 +1,14 @@
+from api import PetFriends
+from settings import valid_email, valid_password
+
+pf= PetFriends()
+
+def test_get_api_key_for_valid_user(email=valid_email, password=valid_password):
+    status, result=pf.get_api_key(email, password)
+    assert status==200
+    assert 'key' in result
+def test_get_all_pets_with_valid_key(filter=''):
+    _, auth_key=pf.get_api_key(valid_email, valid_password)
+    status, result=pf.get_list_of_pets(auth_key, filter)
+    assert status==200
+    assert result len(result['pets'])>0
