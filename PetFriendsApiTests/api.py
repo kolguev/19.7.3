b9fa335a9d8d5d7e0550ffa744ea94ed7d22659c
Aleksey Melnikov <melnikkov@gmail.com>
2020-11-08 14:20:57 +0300

    -

--- PetFriendsApiTests/api.py
+++ PetFriendsApiTests/api.py
@@ -1,11 +1,13 @@
 import requests
+from settings import valid_email, valid_password
+
 class PetFriends:
     def __init__(self):
         self.base_url="http://petfriends1.herokuapp.com/"
     def get_api_key(self, email, password):
         headers={
-            'email': email,
-            'password': password
+            'email': valid_email,
+            'password': valid_password
         }
         res=requests.get(self.base_url+'api/key', headers=headers)
         status=res.status_code
@@ -18,7 +20,7 @@ class PetFriends:
     def get_list_of_pets(self, auth_key, filter):
         headers={'auth_key': auth_key}
         filter={'filter': filter}
-        res=requests.get(self.base_url+'api/pets' headers=headers, params=filter)
+        res=requests.get(self.base_url+'api/pets', headers=headers, params=filter)
         status=res.status_code
         result=""
         try:
--- PetFriendsApiTests/test_pet_friends.py
+++ PetFriendsApiTests/test_pet_friends.py
@@ -4,9 +4,11 @@ from settings import valid_email, valid_password
 pf= PetFriends()
 
 def test_get_api_key_for_valid_user(email=valid_email, password=valid_password):
-    status, result=pf.get_api_key(email, password)
+    status, result=pf.get_api_key(valid_email, valid_password)
     assert status==200
     assert 'key' in result
+
+
 def test_get_all_pets_with_valid_key(filter=''):
     _, auth_key=pf.get_api_key(valid_email, valid_password)
     status, result=pf.get_list_of_pets(auth_key, filter)
